14315dfdd284638dc6288b7a38211feddd1a2a7d
Hellienay Felipe Castanha da Silva Vieira <hellienay.fcsvieira@senacsp.edu.br>
2025-06-09 22:21:59 -0300

    Filmes Bootstrap

--- SITEDEFILME/filmes.php
+++ SITEDEFILME/filmes.php
@@ -1,12 +1,79 @@
 <?php
 include "cabecalho.php";
-include "banner.php";
-include "listadefilmes.php";
-
 ?>
-    
-    <h2>Melhores avaliações</h2>
-    
-<?php 
+<div class="container">
+    <h2 class="display-5">Melhores Avaliações</h2>
+    <div class="row">
+        <div class="col-3 mb-4">
+            <div class="card" style="width: 18rem;">
+                <img src="filmes/filme1.webp" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-text">Jurassic Park</h5>
+                    <p class="card-text">⭐ 10/10</p>
+                    <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3 mb-4">
+
+            <div class="card" style="width: 18rem;">
+                <img src="filmes/filme2.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-text">Karate Kid</h5>
+                    <p class="card-text">
+                        ⭐ 10/10</p>
+                            <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3 mb-4">
+            <div class="card" style="width: 18rem;">
+                <img src="filmes/filme3.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-text">Lilo e Stitch</h5>
+                    <p class="card-text">
+                        ⭐ 10/10</p>
+                            <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                </div>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="filmes/filme4.jpg" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-text">Pecadores</h5>
+                <p class="card-text">
+                    ⭐ 10/10</p>
+                        <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<div class="row">
+    <div class="col-6">
+        <img src="filmes/banguela.png" class="img-fluid">
+    </div>
+    <div class="col-6 align-content-center">
+        <p>Na ilha de Berk, os vikings dedicam a vida a combater e matar dragões. Soluço (Jay Baruchel), filho do chefe Stoico (Gerard Butler), não é diferente. Ele sonha em matar um dragão e provar seu valor ao pai, apesar da descrença geral. Um dia, por acaso, ele acerta um dragão que jamais foi visto, chamado Fúria da Noite. Ao procurá-lo, no dia seguinte, Soluço não consegue matá-lo e acaba soltando-o. Só que ele perdeu parte da cauda e, com isso, não consegue mais voar. Soluço passa a trabalhar em um artefato que possa substituir a parte perdida e, aos poucos, se aproxima do dragão. Só que, paralelamente, Stoico autoriza que o filho participe do treino para dragões, cuja prova final é justamente matar um dos animais.</p>
+        <a href="https://youtube.com" class="btn btn-success btn-lg">ASSISTA AO TRAILER</a>
+    </div>
+</div>
+<div class="row">
+    <div class="col-6 align-content-center">
+        <p>Relâmpago McQueen, um carro de corridas ambicioso, sonha em vencer a Copa Pistão logo em sua estreia. Porém, sua arrogância o leva a um empate na corrida final, forçando uma prova de desempate na Califórnia. No caminho, ele se perde e acaba em Radiator Springs, uma cidade esquecida. Lá, é forçado a consertar os danos que causou e, aos poucos, aprende o valor da amizade, humildade e do trabalho em equipe.</p>
+        <a href="https://youtube.com" class="btn btn-success btn-lg">ASSISTA AO TRAILER</a>
+    </div>
+    <div class="col-6">
+        <img src="filmes/carros.webp" class="img-fluid">
+    </div>
+</div>
+
+
+
+</div>
+</body>
+
+</html>
+<?php
 include "rodape.php";
-?>    
+?>
--- SITEDEFILME/listadefilmes.php
+++ SITEDEFILME/listadefilmes.php
@@ -1,39 +1,80 @@
 <?php
 include "cabecalho.php";
 ?>
-<div class="container">
-    <h2 class="display-5">Melhores Avaliações</h2>
-
-    <div class="lista-filmes">
-        <div class="row">
-            <div class="col-3">
-                <img src="filmes/filme1.webp">
-                <a href="umfilme.php">
-                    <h3>Jurassic Park</h3>
-                </a>
-                <span>⭐ 10/10</span>
-            </div>
-            <div class="col">
-                <img src="filmes/filme2.jpg">
-                <h3>Karate Kid Legends</h3>
-                <span>⭐ 10/10</span>
-            </div>
-            <div class="col">
-                <img src="filmes/filme3.jpg">
-                <h3>Lilo e Stitch</h3>
-                <span>⭐ 10/10</span>
-            </div>
-            <div class="col">
-                <img src="filmes/filme4.jpg">
-                <h3>Pecadores</h3>
-                <span>⭐ 10/10</span>
-            </div>
-        </div>
-    </div>
+<h2>Todos os filmes</h2>
+<div class="row">
+<div class="lista-filmes mt-5">
+        <<div class="card" style="width: 18rem;">
+  <img src="filmes/filme1.webp" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Jurassic Park</h5>
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-primary">Ver Trailer</a>
+  </div>
 </div>
-</body>
+<div class="card" style="width: 18rem;">
+  <img src="https://eskipaper.com/images/madagascar-1.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Madagascar</h5>
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-primary">Ver Trailer</a>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <img src="https://th.bing.com/th/id/OIP.EI4ngqI5tkdK9J0pB28t6gHaK-?r=0&rs=1&pid=ImgDetMain" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Top Gun</h5>
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-primary">Ver Trailer</a>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <img src="https://th.bing.com/th/id/OIP.Xax6q1Obp1KoHYaDIy6jkwHaLH?r=0&rs=1&pid=ImgDetMain" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Batman</h5>
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-primary">Ver Trailer</a>
+  </div>
+</div>
+    </div>
+    <div class="lista-filmes mt-5">
+    <div class="card" style="width: 18rem;">
+  <img src="https://rd1.com.br/wp-content/uploads/2015/04/Imagem-do-Homem-Aranha.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Homem Aranha</h5>
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-primary">Ver Trailer</a>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <img src="filmes/filme4.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Pecadores</h5>
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-primary">Ver Trailer</a>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <img src="filmes/filme3.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Lilo e Stitch</h5>
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-primary">Ver Trailer</a>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <img src="filmes/filme2.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Karate Kid</h5>
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-primary">Ver Trailer</a>
+  </div>
+</div>
+    
+</div>
 
-</html>
-<?php
-include "rodape.php";
-?>
+<?php include "rodape.php"; ?>
+
+
+
+
