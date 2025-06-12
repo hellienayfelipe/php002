4cf02c328e8f923cf62751a47d4dda364dc2e283
Hellienay Felipe Castanha da Silva Vieira <hellienay.fcsvieira@senacsp.edu.br>
2025-06-11 22:21:34 -0300

    php e mysql

--- SITEDEFILME/filmes.php
+++ SITEDEFILME/filmes.php
@@ -3,8 +3,22 @@ include "cabecalho.php";
 ?>
 <div class="container">
     <h2 class="display-5">Melhores Avaliações</h2>
-    <div class="row">
-        <div class="col-3 mb-4">
+    <div class="row mb-5">
+        <?php
+        $servidor = 'localhost';
+        $bd = 'movieflix';
+        $usuario = 'root';
+        $senha = '';
+
+        $conexao = mysqli_connect($servidor, $usuario, $senha, $bd);
+        if(!$conexao){
+            die("deu ruim" . mysqli_connect_error());
+        }
+
+        echo "deu bom";
+        ?>
+    
+    <div class="col-3 mb-4">
             <div class="card" style="width: 18rem;">
                 <img src="filmes/filme1.webp" class="card-img-top">
                 <div class="card-body">
@@ -22,7 +36,7 @@ include "cabecalho.php";
                     <h5 class="card-text">Karate Kid</h5>
                     <p class="card-text">
                         ⭐ 10/10</p>
-                            <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                    <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
                 </div>
             </div>
         </div>
@@ -33,7 +47,7 @@ include "cabecalho.php";
                     <h5 class="card-text">Lilo e Stitch</h5>
                     <p class="card-text">
                         ⭐ 10/10</p>
-                            <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                    <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
                 </div>
             </div>
         </div>
@@ -43,7 +57,7 @@ include "cabecalho.php";
                 <h5 class="card-text">Pecadores</h5>
                 <p class="card-text">
                     ⭐ 10/10</p>
-                        <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
             </div>
         </div>
     </div>
@@ -53,6 +67,7 @@ include "cabecalho.php";
     <div class="col-6">
         <img src="filmes/banguela.png" class="img-fluid">
     </div>
+
     <div class="col-6 align-content-center">
         <p>Na ilha de Berk, os vikings dedicam a vida a combater e matar dragões. Soluço (Jay Baruchel), filho do chefe Stoico (Gerard Butler), não é diferente. Ele sonha em matar um dragão e provar seu valor ao pai, apesar da descrença geral. Um dia, por acaso, ele acerta um dragão que jamais foi visto, chamado Fúria da Noite. Ao procurá-lo, no dia seguinte, Soluço não consegue matá-lo e acaba soltando-o. Só que ele perdeu parte da cauda e, com isso, não consegue mais voar. Soluço passa a trabalhar em um artefato que possa substituir a parte perdida e, aos poucos, se aproxima do dragão. Só que, paralelamente, Stoico autoriza que o filho participe do treino para dragões, cuja prova final é justamente matar um dos animais.</p>
         <a href="https://youtube.com" class="btn btn-success btn-lg">ASSISTA AO TRAILER</a>
@@ -66,6 +81,44 @@ include "cabecalho.php";
     <div class="col-6">
         <img src="filmes/carros.webp" class="img-fluid">
     </div>
+</div>
+
+<button type="button" class="btn btn-warning btn-lg mt-5 fs-5 f2-bold text-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    Precisa de Ajuda? Clique Aqui!
+</button>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Ajuda</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>Entre em contato através dos nossos canais de comunicação:</p>
+        <p>Email: filme@filme.com.br <br> Whatsapp: (11)9999999-9999 <br> <a href="contato.php">Formulario de contato</a> </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<button type="button" class="btn btn-danger btn-lg mt-5 fs-5 f2-bold text-primary" data-bs-toggle="modal" data-bs-target="#meuModal">
+    Veja Nossas Noticias
+</button>
+
+<div class="modal fade" id="meuModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Meu Modal</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>Veja as noticias!</p>
+        <p> <a href="listadenoticias.php">Clique Aqui!</a> </p>
+      </div>
+    </div>
+  </div>
 </div>
 
 
--- SITEDEFILME/listadenoticias.php
+++ SITEDEFILME/listadenoticias.php
@@ -1,32 +1,75 @@
 <?php
 include "cabecalho.php";
 ?>
-
-<h2>Todas as notícias</h2>
-<div class="lista-noticias">
-    <div class="card-noticia">
-        <img src="filmes/noticia1.png">
-        <h3>Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme</h3>
-    </div>
-    <div class="card-noticia">
-        <img src="filmes/noticia1.png">
-        <h3>Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme</h3>
-    </div>
-    <div class="card-noticia">
-        <img src="filmes/noticia1.png">
-        <h3>Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme</h3>
-    </div>
-    <div class="card-noticia">
-        <img src="filmes/noticia1.png">
-        <h3>Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme</h3>
-    </div>
-    <div class="card-noticia">
-        <img src="filmes/noticia1.png">
-        <h3>Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme</h3>
-    </div>
-    <div class="card-noticia">
-        <img src="filmes/noticia1.png">
-        <h3>Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme</h3>
+<div class="container">
+    <h2>Todas as notícias</h2>
+    <div class="row">
+        <div class="col-3 mb-4">
+            <div class="card" style="width: 18rem;">
+                <img src="filmes/noticia1.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme
+                    </p>
+                    <a href="#" class="btn btn-primary">Ver Noticia</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3 mb-4">
+            <div class="card" style="width: 18rem;">
+                <img src="filmes/noticia1.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme
+                    </p>
+                    <a href="#" class="btn btn-primary">Ver Noticia</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3 mb-4">
+            <div class="card" style="width: 18rem;">
+                <img src="filmes/noticia1.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme
+                    </p>
+                    <a href="#" class="btn btn-primary">Ver Noticia</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3 mb-4">
+            <div class="card" style="width: 18rem;">
+                <img src="filmes/noticia1.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme
+                    </p>
+                    <a href="#" class="btn btn-primary">Ver Noticia</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3 mb-4">
+            <div class="card" style="width: 18rem;">
+                <img src="filmes/noticia1.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme
+                    </p>
+                    <a href="#" class="btn btn-primary">Ver Noticia</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3 mb-4">
+            <div class="card" style="width: 18rem;">
+                <img src="filmes/noticia1.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme
+                    </p>
+                    <a href="#" class="btn btn-primary">Ver Noticia</a>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <?php
